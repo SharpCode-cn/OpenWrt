713ea9dc2db71a0bda8f01ba9fc6bf897b202f39
SharpCode <46841497+SharpCode-cn@users.noreply.github.com>
2023-02-09 08:59:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,6 @@
 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+#修改默认内核
+sed -i 's/PATCHVER:=5.15/PATCHVER:=5.10/g' target/linux/x86/Makefile
